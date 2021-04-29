b293820c2c94c453fe30673e8799906c4f54a218
italoiung <italoiung@gmail.com>
2021-04-29 00:04:37 -0300

    fix: episode error handler

--- pages/[id].js
+++ pages/[id].js
@@ -1,15 +1,32 @@
 import { NextSeo } from 'next-seo'
+import styled from 'styled-components'
 
 import Film from '../components/Organisms/Film'
 
+const Error = styled.h2`
+  text-align: center;
+  font-size: 2rem;
+`
+
 const Episode = ({ film }) => (
   <main>
-    <NextSeo title={film.title} />
-    <Film {...film} />
+    {!film &&
+      <Error>Episode not found!</Error>
+    }
+    {film &&
+      <>
+        <NextSeo title={film.title} />
+        <Film {...film} />
+      </>
+    }
   </main>
 )
 
 Episode.getInitialProps = async ({ query }) => {
+  if (!query.id)
+    return {
+      film: null
+    }
   const episodeId = query.id.split('-')[1]
   // API doesn't work properly to fetch single episode by id
   // const res = await fetch(`https://swapi.dev/api/films/${episodeId}`)
